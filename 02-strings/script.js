20d3a9ec3a54f1e72b7abc185929092d1be19b55
MicheleCasani <casanimichele@gmail.com>
2025-03-14 14:06:59 +0100

    [UPD[

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,12 +1,16 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+let name = 'Michele';
+console.log(name);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number = 23
+let text = 'ciao'
+let booleano = true
+console.log (number, text, booleano)
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
